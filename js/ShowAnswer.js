a233ae654d4687ee82fe3f9a022ab092260a098a
oladokunlt <lukmanoladokun86@gmail.com>
2024-07-11 10:45:46 +0100

    finished functionality

--- js/ShowAnswer.js
+++ js/ShowAnswer.js
@@ -1,18 +1,30 @@
-// alert("Welcome Class");
+// get all the sections into an array
+const sections = document.querySelectorAll(".section");
 
-const firstEl = document.querySelector(".first");
-const answerEl = document.querySelector(".answer");
-const iconEl = document.getElementById("plus");
+console.log(sections); // [section1, section2, section3]
 
-firstEl.addEventListener("click", toggleShowAnswer);
+// loop through all the sections one after the other
+for (let i = 0; i < sections.length; i++) {
+  const element = sections[i]; // assign each section as element
+  element.addEventListener("click", toggleShowAnswer);
 
-function toggleShowAnswer() {
-  answerEl.classList.toggle("open");
+  // function to run on click event
+  function toggleShowAnswer() {
+    const iconEl = element.querySelector("#plus");
+    const answerEl = element.querySelector(".answer");
 
-  const condition = answerEl.classList.contains("open");
-  if (condition) {
-    iconEl.src = "./assets/images/icon-plus.svg";
-  } else {
-    iconEl.src = "./assets/images/icon-minus.svg";
+    // add or remove class "opened" to answerEl
+    answerEl.classList.toggle("opened");
+
+    // return true if answerEl contain "opened" else false
+    const condition = answerEl.classList.contains("opened");
+
+    // Change the icons to minus if answer is open
+    // Change the icons to plus if hidden is open
+    if (condition) {
+      iconEl.src = "./assets/images/icon-minus.svg";
+    } else {
+      iconEl.src = "./assets/images/icon-plus.svg";
+    }
   }
 }
